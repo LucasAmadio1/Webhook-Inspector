244ad2e7ba4199a86f6431fcf9b0f448ba0f3eab
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-11-02 14:32:09 -0300

    create api structure

--- api/src/env.ts
+++ api/src/env.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
 	NODE_ENV: z
 		.enum(["development", "production", "test"])
 		.default("development"),
+	DATABASE_URL: z.url(),
 });
 
 export const env = envSchema.parse(process.env);
